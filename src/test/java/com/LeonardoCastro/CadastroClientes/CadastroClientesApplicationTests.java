070537123842c0786133d612e3b0e8673b59ea82
leonardodecastrodasilveira <dev.leonardocastro@gmail.com>
2023-07-17 08:39:49 -0300

    project created

---
+++ src/test/java/com/LeonardoCastro/CadastroClientes/CadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeonardoCastro.CadastroClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
